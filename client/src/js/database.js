5bf56ff26e0df3ba9295f8f0f8032eb7436345af
mshaari <michael.shaari@gmail.com>
2023-04-07 17:18:36 -0400

    Fixing data persistence issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,58 +14,48 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  try {
-    
-    console.log('Post to the database');
 
-    // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('jate', 1);
-  
-    // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('jate', 'readwrite');
-  
-    // Open up the desired object store.
-    const store = tx.objectStore('jate');
-  
-    // Use the .add() method on the store and pass in the content.
-    const request = store.put({ id: 1, value: content });
-      
-    // Get confirmation of the request.
-    const result = await request;
-    console.log('🚀 - data saved to the database', result);
+  console.log('PUT to the database');
 
-  } catch (err) {
-    console.error('putDb not implemented', err);
-  }
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
 }
 
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
 
-  try {
+  console.log('GET from the database');
 
-    console.log('GET from the database');
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
 
-    // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('jate', 1);
-  
-    // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('jate', 'readonly');
-  
-    // Open up the desired object store.
-    const store = tx.objectStore('jate');
-  
-    // Use the .getAll() method to get all data in the database.
-    const request = store.getAll();
-  
-    // Get confirmation of the request.
-    const result = await request;
-    console.log('result.value', result);
-    return result;
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readonly');
 
-  } catch (err) {
-    console.error('putDb not implemented', err);
-  }
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
 };
 
 // Call the function that initiates the IndexedDB database
